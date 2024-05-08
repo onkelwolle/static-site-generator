eb42390ef0244385a32ba0cf67078c0056156246
Wolfgang Zimmermann <wolfgang@zimmermann.red>
2024-05-08 12:04:39 +0200

    add htmlnode class and tests for it

---
+++ src/test_htmlnode.py
@@ -0,0 +1,32 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+
+    def test_to_html_props(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
+
+    def test_repr(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(div, Hello, world!, children: None, {'class': 'greeting', 'href': 'https://boot.dev'})",
+        )
+
+if __name__ == "__main__":
+    unittest.main()
