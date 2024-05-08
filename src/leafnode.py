558939ec9baf9075ca099ec5bf749ffc859ac873
Wolfgang Zimmermann <wolfgang@zimmermann.red>
2024-05-08 21:24:09 +0200

    add text node to html func

--- src/leafnode.py
+++ src/leafnode.py
@@ -2,7 +2,7 @@ from htmlnode import HTMLNode
 
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, children=None, props=None):
+    def __init__(self, tag=None, value=None, props=None):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,15 @@
+from leafnode import LeafNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
 class TextNode:
+
     def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
@@ -11,3 +22,19 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(self, text_node):
+    if self.text_type is text_type_text:
+        return LeafNode(None, text_node.text)
+    elif self.text_type is text_type_bold:
+        return LeafNode("b", text_node.text)
+    elif self.text_type is text_type_italic:
+        return LeafNode("i", text_node.text)
+    elif self.text_type is text_type_code:
+        return LeafNode("code", text_node.text)
+    elif self.text_type is text_type_link:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif self.text_type is text_type_image:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
