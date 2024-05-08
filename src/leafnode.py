7f13eadc3f9db9cb5911532b62466ec4a100c3a8
Wolfgang Zimmermann <wolfgang@zimmermann.red>
2024-05-08 12:56:40 +0200

    add leafnode and to html conversion, test the func

---
+++ src/leafnode.py
@@ -0,0 +1,12 @@
+from htmlnode import HTMLNode
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode doesn't have a value")
+        elif self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
