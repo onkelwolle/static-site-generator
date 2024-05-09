d63aed3a0ed83e298aee0a9754456046487a68bd
Wolfgang Zimmermann <wolfgang@zimmermann.red>
2024-05-09 12:33:05 +0200

    add inline markdown file with split nodes delimiter func and some tests

---
+++ src/inline_markdown.py
@@ -0,0 +1,27 @@
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+)
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    list_of_text_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            list_of_text_nodes.append(node)
+        else:
+            split_node = node.text.split(delimiter)
+            if len(split_node) % 2 == 0:
+                raise ValueError("Invalid Markdown syntax, section not closed")
+            for i in range(len(split_node)):
+                if split_node[i] == "":
+                    continue
+                if i % 2 == 0:
+                    list_of_text_nodes.append(
+                        TextNode(split_node[i], text_type_text))
+                else:
+                    list_of_text_nodes.append(
+                        TextNode(split_node[i], text_type))
+    return list_of_text_nodes
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,12 @@
 import unittest
 
-from textnode import TextNode
+from textnode import (TextNode,
+                      text_type_text,
+                      text_type_bold,
+                      text_type_italic,
+                      text_type_code,
+                      text_type_link,
+                      text_type_image)
 
 
 class TestTextNode(unittest.TestCase):
