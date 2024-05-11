76344e524e58abd13750d6ca012ec1a604f258e1
Wolfgang Zimmermann <wolfgang@zimmermann.red>
2024-05-11 17:21:59 +0200

    add recurive generator function and use it in main

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from block_markdown import markdown_to_html_node
 
 
@@ -37,3 +38,18 @@ def generate_page(from_path, template_path, dest_path):
     dest_file = open(dest_path, "w")
     dest_file.write(replaced_template)
     dest_file.close()
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    items_in_dir = os.listdir(dir_path_content)
+    for item in items_in_dir:
+        from_path = os.path.join(dir_path_content, item)
+        to_path = os.path.join(dest_dir_path, item)
+        if os.path.isfile(from_path):
+            to_path = Path(to_path).with_suffix(".html")
+            generate_page(from_path, template_path, to_path)
+        else:
+            generate_pages_recursive(from_path, template_path, to_path)
--- src/main.py
+++ src/main.py
@@ -2,14 +2,14 @@ import os
 import shutil
 
 from copyfile import copy_files
-from generate_page import generate_page
+from generate_page import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
 
-content_path = "./content/index.md"
+content_path = "./content"
 template_path = "./template.html"
-target_path = "./public/index.html"
+target_path = "./public"
 
 
 def main():
@@ -18,7 +18,7 @@ def main():
         shutil.rmtree(dir_path_public)
     copy_files(dir_path_static, dir_path_public)
 
-    generate_page(content_path, template_path, target_path)
+    generate_pages_recursive(content_path, template_path, target_path)
 
 
 if __name__ == '__main__':
